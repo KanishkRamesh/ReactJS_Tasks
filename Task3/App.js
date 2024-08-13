15cf62234293e6419e678f0a16cd7945aab756cd
Kanishk R <kanishkrameshdev@gmail.com>
2024-08-14 01:25:06 +0530

    Add files via upload

---
+++ Task3/App.js
@@ -0,0 +1,18 @@
+
+import logo from './logo.svg';
+import './Style3.css';
+
+import Task3 from './Components/Task3';
+
+function App() {
+  return (
+    
+   
+    <>
+    <Task3/>
+    
+    </>
+  );
+}
+
+export default App;
