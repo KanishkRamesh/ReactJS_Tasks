18e4d8926dbd86980b9339123a6017e3fd3447df
Kanishk R <kanishkrameshdev@gmail.com>
2024-08-14 01:16:57 +0530

    Add files via upload

---
+++ Task2/Task2.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import Img1 from '../Images/MI.png';
+import Img2 from "../Images/rcb.webp";
+
+function Task2(){
+return(
+
+    <>
+  
+  <div className="Container">
+    <div className="Card">
+        <h1>INDIAN SUPER LEAGUE</h1>
+      <img src={Img1} className="img1"/>
+      <img src={Img2} className="img2"/>
+    </div>
+  </div>
+
+    </>
+)
+
+}
+export default Task2;
