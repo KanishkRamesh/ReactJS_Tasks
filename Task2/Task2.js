b345406bcd93f993321d1db8e9708381acb50e41
Kanishk R <kanishkrameshdev@gmail.com>
2024-08-20 17:09:30 +0530

    Update Task2.js

--- Task2/Task2.js
+++ Task2/Task2.js
@@ -1,7 +1,8 @@
 
+
 import React from "react";
-import Img1 from '../Images/MI.png';
-import Img2 from "../Images/rcb.webp";
+
+
 
 function Task2(){
 return(
@@ -11,8 +12,8 @@ return(
   <div className="Container">
     <div className="Card">
         <h1>INDIAN SUPER LEAGUE</h1>
-      <img src={Img1} className="img1"/>
-      <img src={Img2} className="img2"/>
+      <img src='https://logowik.com/content/uploads/images/rcb-royal-challengers-bangalore7895.logowik.com.webp' className="img1"/>
+      <img src='https://www.pngitem.com/pimgs/m/7-75484_transparent-csk-logo-png-chennai-super-kings-png.png' className="img2"/>
     </div>
   </div>
 
